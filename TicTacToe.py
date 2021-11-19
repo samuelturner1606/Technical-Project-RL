eb5984dc1384e6c7714a762320d1d59ab3c601dd
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2021-11-19 11:12:32 +0000

    deterministic q learning

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,47 +1,31 @@
-from gym import Env, spaces
 import numpy as np
 import random
 
-class TicTacToe(Env):
+class TicTacToe:
     def __init__(self):
-        self.observation_space = spaces.Discrete(3**9)
-        self.action_space = spaces.Discrete(9)
         self.epsilon = 0.1  # exploration rate
-        self.alpha = 0.1  # learning rate
+        self.alpha = 0.9  # learning rate
         self.gamma = 1  # disount factor
-        self.q_table = np.zeros([self.observation_space.n, self.action_space.n])
-
-    def step(self, action):
+        self.Q = {(0,0,0,0,0,0,0,0,0): {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0}} # { states : {actions : values }
+        
+    def step(self,action):
         self.state[action] = self.player # applies action
-        reward, done = self._is_game_over()
-        info = {} # placeholder
-        self.player *= -1 # changes player
+        reward, done = self.is_game_over(self.state)
+        info = None
         return self.state, reward, done, info
 
     def reset(self):
         self.state = np.zeros(9, dtype=int) # [0 0 0 0 0 0 0 0 0]
         self.player = 1 # 'player X' = 1, 'player O' = -1
-        return self.state
+        return np.copy(self.state)
 
-    def render(self, reward):
-        print('\nstate:\n'+str(self.state.reshape([3,3]))+'\nreward: '+str(reward))
+    def render(self, state, reward):
+        print('\nState:\n'+str(np.reshape(state,[3,3]))+'\nreward: '+str(reward))
+        print('\nQ table: '+str(self.Q))
         return
     
-    def _choose_action(self):
-        possible_actions = np.nonzero(self.state == 0)[0]
-        if random.random() < self.epsilon:
-            action = random.choice(possible_actions)
-        else:
-            action, max_value = None, None
-            for possible_action in possible_actions: # could rewrite following code to use argmax?
-                value = self.q_table[(self.state, possible_action)]
-                if max_value is None or max_value < value:
-                    max_value = value
-                    action = possible_action
-        return action
-
-    def _is_game_over(self):
-        state = self.state.reshape([3,3])
+    def is_game_over(self, state):
+        state = np.reshape(state, [3,3])
         S = []
         for i in range(3):
             S.append( sum(state[i,:]) )  # rows
@@ -60,24 +44,56 @@ class TicTacToe(Env):
             reward = 0
         else: # not ended
             done = False
-            reward = None
+            reward = 0
         return reward, done
     
-    def _q_update(self, state, action, reward, new_state, done):
-        #new_q_val = q_val + self.alpha * (reward + self.gamma * max_next_q_val - q_val)
+    def bellman_update(self, old_state, action, reward, state, done):
+        q = self.Q[tuple(old_state)][action]
+        if done:
+            td = 0
+        else:
+            best_next_action, best_next_q = self.best_action(state)
+            td = self.gamma*best_next_q - q # temporal difference
+        # Bellman optimality equation
+        self.Q[tuple(old_state)][action] = q + self.alpha * (reward + td)
         return
+
+    def best_action(self, state):
+        best_action, best_q = None, None
+        for action in self.possible_actions(state):
+            q = self.Q[tuple(state)][action]
+            if self.player == 1: # maximise q
+                if best_q is None or best_q < q:
+                    best_q = q
+                    best_action = action
+            else: # minimise q
+                if best_q is None or best_q > q:
+                    best_q = q
+                    best_action = action
+        return best_action, best_q
+    
+    def possible_actions(self, state):
+        return np.nonzero(state == 0)[0]
+    
+    def greedy_policy(self, state):
+        if random.random() < self.epsilon:
+            action = random.choice(self.possible_actions(state))
+        else:
+            action, best_q = self.best_action(state)
+        return action
+
 
 if __name__ == "__main__":
     env = TicTacToe()
-    epochs = 10
-    for episode in range(epochs):
-        state = env.reset()
-        while True:
-            action = env._choose_action()
-            new_state, reward, done, info = env.step(action)
-            env._q_update(state, action, reward, new_state, done)
-            state = new_state
+    for episode in range(20):
+        old_state = env.reset()
+        for action in range(9):
+            state, reward, done, info = env.step(action)
+            if tuple(state) not in env.Q and not done:
+                    env.Q[tuple(state)] = dict.fromkeys(env.possible_actions(state), 0)
+            env.bellman_update(old_state, action, reward, state, done)
+            old_state = np.copy(state)
+            env.player *= -1 # change player
             if done:
-                env.render(reward)
+                env.render(state, reward)
                 break
-    print('\nq table: '+str(env.q_table))
