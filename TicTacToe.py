3a6c143949bcb746b2988b992dd75c164f683067
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2021-11-25 17:29:39 +0000

    implemented training and testing cycles

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,11 +1,12 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 class TicTacToe:
     def __init__(self):
-        self.epsilon = 0.5  # exploration rate
-        self.alpha = 0.9  # learning rate
-        self.gamma = 1  # disount factor
+        self.epsilon = 1  # exploration rate
+        self.alpha = 0.5  # learning rate
+        self.gamma = 0.9  # disount factor
         self.Q = {(0,0,0,0,0,0,0,0,0): {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0}} # { states : {actions : values }
         
     def step(self,action):
@@ -54,7 +55,8 @@ class TicTacToe:
             best_next_action, best_next_q = self.best_action(state)
             td = self.gamma*best_next_q - q # temporal difference
         # Bellman optimality equation
-        self.Q[tuple(old_state)][action] = q + self.alpha * (reward + td)
+        new_q = q + self.alpha * (reward + td)
+        self.Q[tuple(old_state)][action] = round(new_q, 2)
         return
 
     def best_action(self, state):
@@ -81,21 +83,84 @@ class TicTacToe:
             action, best_q = self.best_action(state)
         return action
 
+    def grow_Q(self, state, done):
+        if not done and tuple(state) not in self.Q:
+                        self.Q[tuple(state)] = dict.fromkeys(self.possible_actions(state), 0)
+        return
+
+    def train(self,epochs):
+        for i in range(epochs):
+            old_state = self.reset()
+            while True:
+                action = self.greedy_policy(old_state)
+                state, reward, done, info = self.step(action)
+                self.grow_Q(state, done)
+                self.bellman_update(old_state, action, reward, state, done)
+                old_state = np.copy(state)
+                self.player *= -1 # change player
+                if done:
+                    self.epsilon = round(self.epsilon*0.99, 5)
+                    # self.render(state, reward)
+                    break
+        return
+    
+    def test(self,epochs):
+        results = []
+        for i in range(epochs):
+            state = self.reset()
+            while True:
+                if self.player == 1:
+                    action, best_q = self.best_action(state)
+                else:
+                    action = random.choice(self.possible_actions(state))
+                state, reward, done, info = self.step(action)
+                self.grow_Q(state, done)
+                self.player *= -1
+                if done:
+                    results.append(reward)
+                    break
+        win_rate = len([x for x in results if x == 1])/epochs
+        draw_rate = len([x for x in results if x == 0])/epochs
+        loss_rate = len([x for x in results if x == -1])/epochs
+        return win_rate, draw_rate, loss_rate
+    
+    def average_q(self):
+        q_values = []    
+        for action_value_pair in env.Q.values():
+            for value in action_value_pair.values():
+                q_values.append(value)
+        avg = round(np.mean(q_values),5)
+        return avg
 
 if __name__ == "__main__":
     env = TicTacToe()
-    for episode in range(1000):
-        old_state = env.reset()
-        while True:
-            action = env.greedy_policy(old_state)
-            state, reward, done, info = env.step(action)
-            if tuple(state) not in env.Q and not done:
-                    env.Q[tuple(state)] = dict.fromkeys(env.possible_actions(state), 0)
-            env.bellman_update(old_state, action, reward, state, done)
-            old_state = np.copy(state)
-            env.player *= -1 # change player
-            if done:
-                env.epsilon*0.9
-                env.render(state, reward)
-                break
-print('\nQ table: '+str(env.Q))
+    avg_q_values, win_rates, draw_rates, loss_rates = [], [], [], []
+    train_n, test_n, cycles = 1000, 100, 1000
+
+    for i in range(cycles):
+        env.train(train_n)
+        avg_q_values.append(env.average_q())
+        win_rate, draw_rate, loss_rate = env.test(test_n)
+        win_rates.append(win_rate)
+        draw_rates.append(draw_rate)
+        loss_rates.append(loss_rate)
+
+    x = np.arange(train_n, train_n*cycles+1, train_n)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, avg_q_values, 'b')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Average Q(s,a) value')
+    ax.set_title('Performance during training')
+    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.plot(x, win_rates, 'g', label='Win rate')
+    ax.plot(x, draw_rates, 'b', label='Draw rate')
+    ax.plot(x, loss_rates, 'r', label='Loss rate')
+    plt.ylim([0, 1])
+    ax.set_xlabel('Training epochs')
+    ax.set_ylabel('Percentage')
+    ax.set_title('Performance against random opponent')
+    ax.legend()
+    plt.show()
