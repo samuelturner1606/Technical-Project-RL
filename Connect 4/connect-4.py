d4b8da69b3324f5d3d6f704fbe9ae684f5405f05
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-01-21 20:19:11 +0000

    code clarity and performance improvements

--- Connect 4/connect-4.py
+++ Connect 4/connect-4.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-import timeit
 
 @dataclass 
 class State():
@@ -11,31 +10,25 @@ class State():
         '''Find all possible next moves given the current state'''
         overlap = self.bitboard1 | self.bitboard2 # find all piece positions
         shifted_up = (overlap << (COLS+1)) | BOTTOM_BITMASK # bitshift up a row and fill in empty bottom row
-        return (shifted_up & ~overlap & FULL_BITMASK)
+        return ~overlap & shifted_up & FULL_BITMASK
     
     def to_mailbox(self) -> str:
         '''Convert the current state into mailbox representation'''
-        board1 = bin(self.bitboard1)[2:].zfill(BYTE_LENGTH)
-        board2  = bin(self.bitboard2)[2:].zfill(BYTE_LENGTH)
-        mailbox = [str(int(board2[bit]+board1[bit],2)) for bit in range(BYTE_LENGTH)]
-        return (''.join(mailbox))
+        bin_string1 = bin(self.bitboard1)[2:].zfill(BYTE_LENGTH)
+        bin_string2  = bin(self.bitboard2)[2:].zfill(BYTE_LENGTH)
+        mailbox = [ str(int(bin_string2[bit] + bin_string1[bit],2)) for bit in range(BYTE_LENGTH) ]
+        return ''.join(mailbox)
     
     def alt_mailbox(self) -> list[int]:
         '''Alternate faster method to convert to mailbox representation'''
-        overlap = self.bitboard1 | self.bitboard2
-        return [(overlap & 1 << b and 1) << (self.bitboard2 & 1 << b and 1) for b in range(BYTE_LENGTH-1,-1,-1)]
-        
-    @staticmethod
-    def to_binary(bitboard: int) -> str:
-        '''Convert a bitboard integer into a binary string'''
-        return bin(bitboard)[2:].zfill(BYTE_LENGTH)
+        # (int & 1 << n and 1) gets the n'th bit of an int
+        return [ (self.bitboard1 & (1 << n) and 1) + 2*(self.bitboard2 & (1 << n) and 1) for n in range(BYTE_LENGTH-1,-1,-1) ]
 
 class Connect4():
     '''A Connect-4 reinforcement learning environment class'''
     def __init__(self):
         self.state = State()
         self.player = 1 # player 1 = 1, player 2 = -1
-        print('\nNew game started')
         self.render()
         
     def step(self, action: int):
@@ -52,7 +45,6 @@ class Connect4():
     def render(self) -> None:
         '''Processes and prints the current state to the terminal'''
         mailbox = self.state.alt_mailbox()
-        #b = mailbox.replace('0','.').replace('1','x').replace('2','o')
         for row in self.make2D(mailbox):
             print(row)
 
@@ -93,36 +85,36 @@ class Connect4():
                 
 
     @staticmethod
-    def make2D(binary: list) -> list[list]:
+    def make2D(board: list) -> list[list]:
         '''Remove bitboard padding and split it by the number of rows'''
-        return [binary[(COLS+1)*i:(COLS+1)*i+COLS] for i in range(ROWS)]
+        return [ board[ (COLS+1)*row : (COLS+1)*row+COLS ] for row in range(ROWS) ]
     
     @staticmethod
     def col_bitmask(move_col: int) -> int:
         '''Convert a move column integer into a bitmask to be used on a bitboard'''
-        a = ['0']*(COLS+1)
-        a[move_col-1]='1'
-        b = ''.join(a) * ROWS
-        return int(b, 2)
+        zeroes_list = ['0']*(COLS+1)
+        zeroes_list[move_col-1]='1'
+        col_bitmask = ''.join(zeroes_list) * ROWS
+        return int(col_bitmask, 2)
     
     @staticmethod
     def is_win(bitboard: int) -> bool:
         '''Check if a bitboard integer has connect four'''
         # Check -
         pairs = bitboard & (bitboard >> 1)
-        if (pairs & (pairs >> 2 * 1)):
+        if pairs & (pairs >> 2):
             return True
         # Check |
         pairs = bitboard & (bitboard >> (COLS+1))
-        if (pairs & (pairs >> 2 * (COLS+1))):
+        if pairs & (pairs >> 2*(COLS+1)):
             return True
         # Check /
         pairs = bitboard & (bitboard >> COLS)
-        if (pairs & (pairs >> 2 * COLS)):
+        if pairs & (pairs >> 2*COLS):
             return True
         # Check \
         pairs = bitboard & (bitboard >> (COLS+2))
-        if (pairs & (pairs >> 2 * (COLS+2))):
+        if pairs & (pairs >> 2*(COLS+2)):
             return True
 
 def main():
@@ -139,6 +131,6 @@ def main():
 if __name__ == "__main__":
     ROWS, COLS = 6, 7
     BYTE_LENGTH = ROWS * (COLS+1)
-    BOTTOM_BITMASK = ( 2**(COLS+1) - 2 ) 
-    FULL_BITMASK = ( 2**(BYTE_LENGTH) - 1 ) # used to remove extra bits from a left bitshift
+    BOTTOM_BITMASK = 2**(COLS+1) - 2
+    FULL_BITMASK = 2**(BYTE_LENGTH) - 1 # used to remove extra bits from a left bitshift
     main()
