28713ee00f2e914d17b73d7b892d2c1a17e768a1
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-03-01 16:53:50 +0000

    passive moves

--- Shobu/simplified_shobu.py
+++ Shobu/simplified_shobu.py
@@ -1,11 +1,10 @@
 DIRECTIONS = {'N':5, 'E':-1, 'S':-5, 'W':1, 'NE':4, 'NW': 6, 'SE': -6, 'SW': -4}
-BITMASK = 0b01111011110111101111
-player = True # player 1 = True, player 2 = False
+BITMASK = 0b01111011110111101111 # bitmask deletes bits that move off the board
 
 class Board:
     '''Bitboard representation of one of the game boards.'''
     def __init__(self) -> None:
-        self.bitboards = [0b1111, 0b01111000000000000000] # index 1 corresponds to the bitboard of player 1
+        self.bitboards = [0b1111, 0b1111000000000000000] # index 1 corresponds to the bitboard of player 1
 
     def mailbox(self) -> list[list[int]]:
         'Transform two bitboards into a 2D mailbox representation and print it.'
@@ -14,11 +13,12 @@ class Board:
         print(*mailbox_2D, sep='\n', end='\n\n')
         return mailbox_2D
     
-    def __hash__(self) -> int:
-        return self.bitboards[1] | self.bitboards[0]
+    def empty(self) -> int:
+        return ~(self.bitboards[0] | self.bitboards[1])
 
 class Shobu:
     def __init__(self) -> None:
+        self.player = True # player 1 = True, player 2 = False
         self.boards = [Board(), Board()]
 
     def render(self, message: str = '') -> None:
@@ -36,29 +36,38 @@ class Shobu:
                 return 1 # player 1 wins
         return 0 # game not over
     
-    def passive_move(self, homeboard: Board, start_square: int, direction: int, distance: int) -> None:
+    def passive_move(self, homeboard: Board, end_square: int, direction: int, distance: int) -> None:
         'Updates a board with a passive move, assuming it is legal.'
-        end_square =  self.bitshift(start_square, direction, distance)
-        homeboard.bitboards[player] ^= start_square|end_square
+        start_square =  self.bitshift(end_square, -direction, distance)
+        homeboard.bitboards[self.player] ^= start_square|end_square
         return
     
-    def legal_passives(self, bits: int, direction: int, distance: int, empty: int):
-        output = {1:{}, 2:{}}
-        for dist in output.keys:
-            for d in DIRECTIONS.keys:
-                nbors = self.bitshift(bits, d, dist) & empty
-                
-        return output
+    def legal_passives(self, homeboard: Board) -> dict[str,dict[int,int]]:
+        '''Calculate the ending squares of legal passive moves in all directions. 
+        To access `moves` from returned dictionary use dictionary[`direction`][`distance`].'''
+        all_legal_moves = {}
+        for key, direction in DIRECTIONS.items():
+            moves = homeboard.bitboards[self.player]
+            legal_moves = {}
+            for distance in [1,2]:
+                moves = homeboard.empty() & self.bitshift(moves, direction, 1)
+                if moves:
+                    legal_moves[distance] = moves
+            if legal_moves:
+                all_legal_moves[key] = legal_moves      
+        return all_legal_moves
     
     @staticmethod
     def bitshift(bits: int, direction: int, distance: int) -> int:
         shift = direction * distance
         if shift > 0:
-            return bits >> shift
+            return (bits >> shift) & BITMASK
         else:
-            return (bits << -shift) & BITMASK # bitmask deletes bits that move off the board
+            return (bits << -shift) & BITMASK
 
-s = Shobu()
-s.render()
-s.passive_move(s.boards[1], 0b0100000000000000000, DIRECTIONS['NW'], 2)
-s.render()
+def view(bits: int, message: str = '') -> None:
+    'For debugging.'
+    mailbox = [ bits & (1 << n) and 1  for n in range(20) ]
+    mailbox_2D = [ mailbox[ 5*row : 5*row+5 ] for row in range(4) ]
+    print(*mailbox_2D, sep='\n', end=f'\n{message}\n')
+    return
