8ed0c8a11336ee39c67c44231e6488e4abbdf317
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-03-28 09:18:01 +0100

    padding

--- shobu/NN.py
+++ shobu/NN.py
@@ -6,12 +6,14 @@ def int_to_tensor(bits: int) -> tf.Tensor:
     bytes = bits.to_bytes(length=3, byteorder='little')
     byte_array = np.frombuffer(bytes, dtype=np.uint8)
     bit_array = np.unpackbits(byte_array, bitorder='little').reshape((4, 6))
-    tensor = tf.constant(bit_array)
+    tensor = tf.constant(bit_array[:, :4])
     return tensor
 
 def tensor_to_int(tensor: tf.Tensor) -> int:
     'Convert a tensor to a bitboard.'
-    flat = np.ravel(tensor)
+    pad = np.zeros((4, 6), dtype=np.uint8)
+    pad[:, :4] = tensor
+    flat = np.ravel(pad)
     pack = np.packbits(flat, bitorder='little')
-    num = int.from_bytes(pack, byteorder='little')   
+    num = int.from_bytes(pack, byteorder='little')
     return num
