84a07dc8b3c1784dd0360b7fe8f0a613c6b35cf3
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-03-31 21:10:16 +0100

    finding legal moves

--- shobu/logic.py
+++ shobu/logic.py
@@ -6,32 +6,15 @@ Board game engine implementation of Shōbu using bitwise operators.
 from random import choice, shuffle
 import numpy as np
 
-DI: dict[str,tuple[int]] = {'N':(-1,0),'NE':(-1,1),'E':(0,1),'SE':(1,1),'S':(1,0),'SW':(1,-1),'W':(0,-1),'NW':(-1,-1) }
-SHIFT2 = {
-    1: {
-        'N':(-1,0),
-        'NE':(-1,1),
-        'E':(0,1),
-        'SE':(1,1),
-        'S':(1,0),
-        'SW':(1,-1),
-        'W':(0,-1),
-        'NW':(-1,-1) }, 
-    2: {
-        'N':(-2,0),
-        'NE':(-2,2),
-        'E':(0,2),
-        'SE':(2,2),
-        'S':(2,0),
-        'SW':(2,-2),
-        'W':(0,-2),
-        'NW':(-2,-2) }
-}
-
 COMBOS = ( # mapping of passive to aggro board combinations
-    { 0: [1, 2], 1: [0, 3]}, # player 2
-    { 2: [0, 3], 3: [1, 2]} # player 1
-)
+    { 0: (1, 2), 1: (0, 3)}, # player 2
+    { 2: (0, 3), 3: (1, 2)} )# player 1
+QUADRANT = ( # Get an object that will slice an nx8x8 np.ndarray into nx4x4, via the indices 0 to 3.
+    (slice(None), slice(0,4), slice(0,4)),
+    (slice(None), slice(0,4), slice(4,8)),
+    (slice(None), slice(4,8), slice(0,4)),
+    (slice(None), slice(4,8), slice(4,8)) )
+SHIFT: dict[str,tuple[int]] = {'N':(-1,0),'NE':(-1,1),'E':(0,1),'SE':(1,1),'S':(1,0),'SW':(1,-1),'W':(0,-1),'NW':(-1,-1)}
 
 def negate(offset: tuple[int]) -> tuple[int]:
     'Returns the shift `offset` in the opposite direction.'
@@ -56,25 +39,7 @@ class State2:
         self.plies:int = 0
 
     def is_terminal(self) -> bool:
-        '''The first player to lose all their stones on any board loses the game. 
-        Reward values 1 = player 1 win, -1 = player 2 win and 0 = game not over.
-        Returns self.done'''
-        if self.plies >= 1000: # Max game length avoids theoretical stalemates
-            self.done = True
-            self.reward = 0
-            self.render('Max game length reached')
-        elif not self.done: # if not already terminal from there being no legal moves or max game length
-            for side in self.boards:
-                for board in side:
-                    if not board[0]:
-                        self.done = True
-                        self.reward = 1 # player 1 wins
-                        break 
-                    elif not board[1]:
-                        self.done = True
-                        self.reward = -1 # player 2 wins
-                        break 
-        return self.done 
+        return
     
     def make_move(self, passive_board: list[int], aggro_board: list[int], passive_end: int, aggro_end: int, direction: int, distance: int) -> None:
         'Update the two boards inplace with the legal passive and aggro moves.'
@@ -104,12 +69,14 @@ class State2:
         'Ask the user for a legal move and update the boards with it.'
         return
 
-
 class State:
-    '4 Shōbu 4x4 boards.'
+    '''4 Shōbu 4x4 boards.
+    @param boards: 2x8x8 np.ndarray or None
+    @param player: 1 for player1, 0 for player2.'''
     def __init__(self, boards: np.ndarray = None, player: int = 1) -> None:
-        self.player = player # player 1 = index 1, player 2 = index 0
-        if boards is None:
+        assert (player==1 or player==0), 'Player parameter incorrect.'
+        self.player = player
+        if boards is None: # creates default starting state
             self.boards = np.zeros(shape=(2,8,8), dtype=np.uint8)
             self.boards[1, -1:0:-4, :] = 1 # player 1 pieces
             self.boards[0, 0:-1:4, :] = 1 # player 2 pieces
@@ -119,28 +86,7 @@ class State:
             
     def render(self) -> None:
         'Prints the current game boards.'
-        return print(2*self.boards[0,:,:] + self.boards[1,:,:])
-    
-    def board(self, num: int) -> np.ndarray:
-        'Get a 2x4x4 board from the 2x8x8 `self.boards`, via the numbers 0 to 3.'
-        if num == 0:
-            return self.boards[:, 0:4, 0:4]
-        elif num == 1:
-            return self.boards[:, 0:4, 4:8]
-        elif num == 2:
-            return self.boards[:, 4:8, 0:4]
-        else:
-            return self.boards[:, 4:8, 4:8]
-    
-    QUADRANT = ( # Get an object that will slice an nx8x8 array into nx4x4, via the numbers 0 to 3.
-        (slice(None), slice(0,4), slice(0,4)),
-        (slice(None), slice(0,4), slice(4,8)),
-        (slice(None), slice(4,8), slice(0,4)),
-        (slice(None), slice(4,8), slice(4,8))
-        ) 
-    def quadrant(self, array: np.ndarray, num: int):
-        
-        return
+        return print(2*self.boards[0,:,:] + self.boards[1,:,:])  
 
     def shift(self, array: np.ndarray, offset: tuple) -> np.ndarray:
         'Returns a copy of an `array` shifted (without rollover) by `offset`.'
@@ -153,23 +99,22 @@ class State:
             new_array[(slice(None),) * axis + (slice(0, o) if o >= 0 else slice(o, None),)] = 0
         return new_array
 
-    def legal_passives(self, p_board: np.ndarray, direction: str) -> tuple[np.ndarray]:
+    def legal_passives(self, p_board: np.ndarray, direction: str) -> tuple[np.ndarray,np.ndarray]:
         '''Find the ending squares of all legal passive moves, for all distances, in a direction, on a given board.
         @param p_board: A 2x4x4 ndarray representing the board to play a passive move on.
         @param direction: Compass direction.
         @return passive1: A 2x4x4 ndarray with a legal passive move, of distance 1, applied to all pieces of the current player.
-        @return passive2: A 2x4x4 ndarray with a legal passive move, of distance 2, applied to all pieces of the current player.
-        '''
+        @return passive2: A 2x4x4 ndarray with a legal passive move, of distance 2, applied to all pieces of the current player.'''
         assert p_board.shape == (2,4,4), 'Passive board not the right shape.'
         player = p_board[self.player, :, :]
         opponent = p_board[1 - self.player, :, :]
         empty = 1 - (player | opponent)
-        offset1 = DI[direction]
+        offset1 = SHIFT[direction]
         passive1 = self.shift(player, offset1) & empty
         passive2 = self.shift(passive1, offset1) & empty
         return passive1, passive2
     
-    def legal_aggros(self, a_board: np.ndarray, direction: str) -> tuple[np.ndarray]:
+    def legal_aggros(self, a_board: np.ndarray, direction: str) -> tuple[np.ndarray,np.ndarray]:
         '''Find the ending squares of all legal aggressive moves, for all distances, in a direction, on a given board.
         @param a_board: A 2x4x4 ndarray representing the board to play a aggressive move on.
         @param direction: Compass direction.
@@ -177,7 +122,7 @@ class State:
         @return aggro2: A 2x4x4 ndarray with a legal aggressive move, of distance 2, applied to all pieces of the current player.
         '''
         assert a_board.shape == (2,4,4), 'Aggressive board not the right shape.'
-        offset1 = DI[direction]
+        offset1 = SHIFT[direction]
         p2 = a_board[self.player, :, :]
         p3 = self.shift(p2, offset1)
         x2 = p2 | a_board[1 - self.player, :, :]
@@ -191,16 +136,22 @@ class State:
         return aggro1, aggro2
 
     def legals(self) -> np.ndarray:
-        '''Find all legal moves, in all directions, on all boards not including board combos.'''
-        output = np.zeros(shape=(32,8,8), dtype=np.uint8)
-        boards = [self.board(i).copy() for i in range(4)]
-        #for index, direction in enumerate(DI):
-            #pass
-        direction = 'N'
-        index = 0
-        for b in COMBOS[self.player]:
-            p1, p2 = self.legal_passives(boards[b], direction)
-        return output
+        'Find all legal moves, in all directions, on all boards.'
+        output = []
+        board = [ self.boards[QUADRANT[i]] for i in range(len(QUADRANT)) ]
+        for direction in SHIFT:
+            block = np.zeros(shape=(4,8,8), dtype=np.uint8)
+            for p in COMBOS[self.player]:
+                p1, p2 = self.legal_passives(board[p], direction)
+                if p1.any():
+                    block[QUADRANT[p]][0:2, :, :] = np.stack((p1,p2))
+                    for a in COMBOS[self.player][p]:
+                        a1, a2 = self.legal_aggros(board[a], direction)
+                        if not p2.any():
+                            a2 = np.zeros_like(a2)
+                        block[QUADRANT[a]][2:4, :, :] = np.stack((a1,a2))
+            output.append(block)
+        return np.concatenate(output)
 if __name__ == '__main__':
     #a = np.random.randint(2, size=(2,8,8), dtype=np.uint8)
     #b1 = a[1,:,:]
@@ -223,10 +174,22 @@ if __name__ == '__main__':
     [1, 0, 1, 0, 0, 1, 1, 0],
     [1, 0, 1, 0, 1, 1, 0, 0]]]
     a = np.asarray(a, dtype=np.uint8)
-
     game = State(a)
     game.render()
-    game.player = 1 - game.player
-    game.legals()
+    x = game.legals()
+    assert x.shape == (32,8,8)
 
 pass
+
+'''
+def board(self, num: int) -> np.ndarray:
+    'Get a 2x4x4 board from the 2x8x8 `self.boards`, via the numbers 0 to 3.'
+    if num == 0:
+        return self.boards[:, 0:4, 0:4]
+    elif num == 1:
+        return self.boards[:, 0:4, 4:8]
+    elif num == 2:
+        return self.boards[:, 4:8, 0:4]
+    else:
+        return self.boards[:, 4:8, 4:8]
+'''
