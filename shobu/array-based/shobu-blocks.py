3ffb605c7f271afdddcf42dda7e12297eb30411e
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-04-15 20:27:32 +0100

    train and save functions

--- shobu/array-based/shobu-blocks.py
+++ shobu/array-based/shobu-blocks.py
@@ -1,9 +1,9 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, Model, Input, initializers, optimizers
+from tensorflow.keras import layers, Model, Input, initializers, optimizers, utils, losses, models
 from logic3 import Game, State, Random
 
-def basic(x, filters=256, kernel_size=3, strides=1, is_input: bool = False):
+def basic(x, filters=256, kernel_size=3, strides=1):
     'Convolutional layer then BatchNormalization and ReLU activation.'
     x = layers.Conv2D(
         filters=filters,
@@ -14,8 +14,6 @@ def basic(x, filters=256, kernel_size=3, strides=1, is_input: bool = False):
         use_bias=False,
         kernel_initializer=initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None)
     )(x)
-    if is_input:
-        x.input_shape=(2, 8, 8)
     x = layers.BatchNormalization(axis=1, momentum=0.999, epsilon=0.001, center=True, scale=False)(x)
     x = layers.ReLU()(x)
     return x
@@ -40,20 +38,28 @@ def bottleneck(tensor, filters=128):
     return x
 
 def make_network():
-    input = Input(shape=(2,8,8), dtype=tf.float32)
-    x = basic(input, kernel_size=3, is_input=True)
+    state = Input(shape=(2,8,8), dtype='float32', name='state')
+    x = basic(input, kernel_size=3)
     x = bottleneck(x)
     x = bottleneck(x)
     x = bottleneck(x)
     x = bottleneck(x)
     x = bottleneck(x)
     x = bottleneck(x)
-    x = layers.Flatten(data_format='channel_first')(x)
+    x = layers.Flatten(data_format='channels_first')(x)
     actor = layers.Dense(16384, activation='softmax', name='actor')(x)
     critic = layers.Dense(1, activation='tanh', name='critic')(x)  
-    model = Model(inputs=input, outputs=[actor, critic])
-    model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=optimizers.Adam()) 
+    model = Model(inputs=state, outputs=[actor, critic])
+    model.compile(
+        optimizer = 'sgd', # optimizers.Adam()
+        loss = {
+            'actor': losses.KLDivergence(), # 'categorical_crossentropy'
+            'critic': losses.CategoricalCrossentropy(from_logits=True), # 'mean_squared_error'
+        },
+        loss_weights = {'actor': 1, 'critc': 1}
+    )
     model.summary()
+    #utils.plot_model(model, "model_diagram.png", show_shapes=True)
     return model
 
 def process_state(state: State):
@@ -62,3 +68,16 @@ def process_state(state: State):
         return np.array([state.boards[1],state.boards[0]], state.boards.dtype)
     else:
         return state.boards
+
+def train():
+    """
+    This function trains the neural network with examples obtained from self-play.
+    - pi is the MCTS informed policy vector for the given board
+    """
+    return model.fit(x = input_boards, y = [target_pis, target_vs], batch_size = 64, epochs = 10)
+
+def save_checkpoint(model, path):
+    model.save(path)
+    del model
+    # Recreate the exact same model purely from the file:
+    return models.load_model(path)
