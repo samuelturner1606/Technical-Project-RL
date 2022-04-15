2326030891dd18a25f29b54ca50f4ed9899c4f60
samuelturner1606 <46319151+samuelturner1606@users.noreply.github.com>
2022-04-15 17:02:47 +0100

    creating network architecture

---
+++ shobu/array-based/shobu-blocks.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, Model, Input, initializers
+
+def basic(x, filters=256, kernel_size=3, strides=1, is_input: bool = False):
+    'Convolutional layer then BatchNormalization and ReLU activation.'
+    x = layers.Conv2D(
+        filters=filters,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding="valid", # 'same' from website
+        data_format='channels_first',
+        use_bias=False,
+        kernel_initializer=initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None)
+    )(x)
+    if is_input:
+        x.input_shape=(2, 8, 8)
+    x = layers.BatchNormalization(axis=1, momentum=0.999, epsilon=0.001, center=True, scale=False)(x)
+    x = layers.ReLU()(x)
+    return x
+
+def bottleneck(tensor, filters=128):
+    'Creates residual block with bottleneck and skip connection.'
+    x = basic(tensor, filters=filters, kernel_size=1, strides=1)
+    x = basic(x, filters=filters, kernel_size=3, strides=1)
+    x = basic(x, filters=filters, kernel_size=3, strides=1)
+    x = layers.Conv2D(
+        filters=2*filters, # 4* from website
+        kernel_size=1,
+        strides=1,
+        padding="valid", # 'same' from website
+        data_format='channels_first',
+        use_bias=False,
+        kernel_initializer=initializers.TruncatedNormal(mean=0.0, stddev=0.01, seed=None)
+    )(x)
+    x = layers.BatchNormalization(axis=1, momentum=0.999, epsilon=0.001, center=True, scale=False)(x)
+    x = layers.Add()([tensor,x]) # skip connection
+    x = layers.ReLU()(x)
+    return x
+
+def make_network():
+    input = Input(shape=(2,8,8), dtype=tf.float32)
+    x = basic(input, kernel_size=3, is_input=True)
+    x = bottleneck(x)
+    x = bottleneck(x)
+    x = bottleneck(x)
+    x = bottleneck(x)
+    x = bottleneck(x)
+    x = bottleneck(x)
+    actor = layers.Dense(16384, activation='softmax', name='actor')(x)
+    critic = layers.Dense(1, activation='tanh', name='critic')(x)  
+    model = Model(inputs=input, outputs=[actor, critic])
+    model.summary()
+    return model
